96165df6d47f5a31a5d4ad1a40916c189ebebc1a
chung.yeoh <chung.yeoh@symphony.com>
2020-05-09 16:59:59 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
 # Pod User App Report
 
+## Overview
+This Python script is built based on the [Symphony Python SDK](https://github.com/SymphonyPlatformSolutions/symphony-api-client-python)
+
+The script will retrieve All Active Users from the pod.
+For each user, it will retrieve list of installed app.
+
+The script will produce one CSV file per app containing list of all the users who have got the app installed.
+
+## Output Columns
+The CSV file will contain following columns:
+- User Name
+- First Name
+- Last Name
+- Email
+- App Subscription
+
+The output file(s) will be saved in the root directory in format - ``App Name (app-id).csv``
+
+## Environment Setup
+This client is compatible with **Python 3.6 or above**
+
+Create a virtual environment by executing the following command **(optional)**:
+``python3 -m venv ./venv``
+
+Activate the virtual environment **(optional)**:
+``source ./venv/bin/activate``
+
+Install dependencies required for this client by executing the command below.
+``pip install -r requirements.txt``
+
+
+## Getting Started
+### 1 - Prepare the service account
+The Python client operates using a [Symphony Service Account](https://support.symphony.com/hc/en-us/articles/360000720863-Create-a-new-service-account), which is a type of account that applications use to work with Symphony APIs. Please contact with Symphony Admin in your company to get the account.
+
+The client currently supports two types of Service Account authentications, they are
+[Client Certificates Authentication](https://symphony-developers.symphony.com/symphony-developer/docs/bot-authentication-workflow#section-authentication-using-client-certificates)
+and [RSA Public/Private Key Pair Authentication](https://symphony-developers.symphony.com/symphony-developer/docs/rsa-bot-authentication-workflow).
+
+**RSA Public/Private Key Pair** is the recommended authentication mechanism by Symphony, due to its robust security and simplicity.
+
+**Important** - The service account must have **User Provisioning** role in order to work.
+
+### 2 - Upload Service Account Private Key
+Please copy the Service Account private key file (*.pem) to the **rsa** folder. You will need to configure this in the next step.
+
+### 3 - Update resources/config.json
+
+To run the bot, you will need to configure **config.json** provided in the **resources** directory. 
+
+**Notes:**
+Most of the time, the **port numbers** do not need to be changed.
+
+You should replace **pod** with your actual Pod URL.
+
+You also need to update based on the service account created above:
+- botPrivateKeyPath (ends with a trailing "/"))
+- botPrivateKeyName
+- botUsername
+- botEmailAddress
+
+Sample:
+
+    {
+      "sessionAuthHost": "<pod>.symphony.com",
+      "sessionAuthPort": 443,
+      "keyAuthHost": "<pod>.symphony.com",
+      "keyAuthPort": 443,
+      "podHost": "<pod>.symphony.com",
+      "podPort": 443,
+      "agentHost": "<pod>.symphony.com",
+      "agentPort": 443,
+      "authType": "rsa",
+      "botPrivateKeyPath":"./rsa/",
+      "botPrivateKeyName": "bot-private-key.pem",
+      "botCertPath": "",
+      "botCertName": "",
+      "botCertPassword": "",
+      "botUsername": "<bot-user>",
+      "botEmailAddress": "<bot-email>",
+      "appCertPath": "",
+      "appCertName": "",
+      "appCertPassword": "",
+      "authTokenRefreshPeriod": "30",
+      "proxyURL": "",
+      "proxyUsername": "",
+      "proxyPassword": "",
+      "podProxyURL": "",
+      "podProxyUsername": "",
+      "podProxyPassword": "",
+      "agentProxyURL": "",
+      "agentProxyUsername": "",
+      "agentProxyPassword": "",
+      "keyManagerProxyURL": "",
+      "keyManagerProxyUsername": "",
+      "keyManagerProxyPassword": "",
+      "truststorePath": ""
+    }
+
+### 4 - Run script
+The script can be executed by running
+``python3 main.py`` 
+
+
+
+# Release Notes
+
+## 0.1
+- Initial Release
+
---
+++ main.py
@@ -0,0 +1,138 @@
+import logging, json, csv
+from datetime import datetime
+from pytz import timezone
+from sym_api_client_python.configure.configure import SymConfig
+from sym_api_client_python.auth.rsa_auth import SymBotRSAAuth
+from sym_api_client_python.clients.sym_bot_client import SymBotClient
+from sym_api_client_python.clients.admin_client import AdminClient
+
+
+def configure_logging():
+    logging.basicConfig(
+            filename='./logs/output.log',
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            filemode='w', level=logging.DEBUG
+    )
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+def main():
+    print('Python Client runs using RSA authentication')
+
+    # Configure log
+    configure_logging()
+
+    # RSA Auth flow: pass path to rsa config.json file
+    configure = SymConfig('./resources/config.json')
+    configure.load_config()
+    auth = SymBotRSAAuth(configure)
+    print('Start Authenticating..')
+    logging.info('Start Authenticating..')
+    auth.authenticate()
+
+    # Initialize SymBotClient with auth and configure objects
+    bot_client = SymBotClient(auth, configure)
+    admin_client = AdminClient(bot_client)
+
+    # Retrieve list of users
+    print('Retrieve All Active Pod Users...')
+    logging.info('Retrieve All Active Pod Users...')
+
+    user_lists = retrieve_all_active_pod_users(admin_client)
+
+    print(f'Retrieved {str(len(user_lists))} users')
+    logging.info(f'Retrieved {str(len(user_lists))} users')
+
+    app_list = dict()
+    # Loop users and get App
+    for user in user_lists:
+        print(f'Getting Apps for {user["userAttributes"]["emailAddress"]}')
+        logging.info(f'Getting Apps for {user["userAttributes"]["emailAddress"]}')
+
+        install_app = retrieve_installed_apps(bot_client, user["userSystemInfo"]["id"])
+
+        for app in install_app:
+            app_id = f"{app['appName']} ({app['appId']})"
+            if app_id not in app_list:
+                app_list[app_id] = []
+
+            u = dict()
+            u['userAttributes'] = user["userAttributes"]
+            if 'products' in app:
+                u['subscription'] = app['products'][0]['name']
+            app_list[app_id].append(u)
+
+    # Print final result
+    print(f'Generating Result Files...')
+    logging.info(f'Generating Result Files...')
+    print_result(app_list)
+
+
+def print_result(app_list):
+    for app_id, user_list in app_list.items():
+        file_name = app_id + ".csv"
+
+        with open(file_name, 'w', newline='', encoding='utf-8-sig') as csvfile:
+            fieldnames = ['User Name',
+                          'First Name',
+                          'Last Name',
+                          'Email',
+                          'App Subscription']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for u in user_list:
+                writer.writerow(
+                        {'User Name': u['userAttributes']['userName'],
+                         'First Name': u['userAttributes']['firstName'],
+                         'Last Name': u['userAttributes']['lastName'],
+                         'Email': u['userAttributes']['emailAddress'],
+                         'App Subscription': u['subscription'] if 'subscription' in u else ''})
+
+    return
+
+
+def retrieve_all_active_pod_users(admin_client):
+    output = admin_client.admin_list_users(skip=0, limit=1000)
+
+    while True:
+        next_out = admin_client.admin_list_users(skip=int(len(output)), limit=1000)
+        if len(next_out) > 0:
+            for index in range(len(next_out)): output.append(next_out[index])
+        else:
+            break
+
+    # Filter out Service Acc and Disabled users
+    final_result = []
+    for u in output:
+        if u["userAttributes"]["accountType"] == "NORMAL" and u["userSystemInfo"]["status"] == "ENABLED":
+            final_result.append(u)
+
+    return final_result
+
+
+def retrieve_installed_apps(bot_client, user_id):
+    output = admin_get_user_features(bot_client, user_id)
+
+    # Filter install apps only
+    final_result = []
+    for o in output:
+        if o["install"]:
+            if 'products' in o:
+                new_prod = []
+                for p in o['products']:
+                    if p['subscribed']:
+                        new_prod.append(p)
+                o['products'] = new_prod
+            final_result.append(o)
+
+    return final_result
+
+
+def admin_get_user_features(bot_client, user_id):
+        url = '/pod/v1/admin/user/{0}/app/entitlement/list'.format(user_id)
+        return bot_client.execute_rest_call("GET", url)
+
+
+if __name__ == "__main__":
+    main()
